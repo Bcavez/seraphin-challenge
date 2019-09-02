b8d48e3be49f4c86dfdd6ca2c4299623facb29cf
Benoit Cavez <benoit.cavez@gmail.com>
2019-09-02 16:38:24 +0200

    add test: making a post request should not return a 404

---
+++ test/routing.js
@@ -0,0 +1,11 @@
+const expect = require('chai').expect;
+
+const app = require('../app.js');
+
+describe('Routes', function() {
+    it('should not return a 404 error when doing a POST request to localhost:8080/api/v1/quote/car-insurance', function() {
+        const path = 'localhost:8080/api/v1/quote/car-insurance'
+        app.listen(8080);
+        expect(app.use(path)).to.not.throw('404 (not found)')
+    });
+});
