754630468d25f6f36e1adc9f003ed883e0413018
Benoit Cavez <benoit.cavez@gmail.com>
2019-09-02 17:55:13 +0200

    added http-chai

--- app.js
+++ app.js
@@ -5,3 +5,5 @@ const app = express();
 const quoteRoutes = require('./routes/quote');
 
 app.listen(8080);
+
+module.exports = app;
--- test/routing.js
+++ test/routing.js
@@ -1,11 +1,21 @@
+const chai = require('chai');
 const expect = require('chai').expect;
+const chaiHttp = require('chai-http');
 
 const app = require('../app');
 
+chai.use(chaiHttp);
+
 describe('Routes', function() {
-    it('should not return a 404 error when doing a POST request to localhost:8080/api/v1/quote/car-insurance', function() {
-        const path = 'localhost:8080/api/v1/quote/car-insurance'
-        app.listen(8080);
-        expect(app.use(path)).to.not.throw('404 (not found)')
+    it('should not return a 404 error when doing a POST request to localhost:8080/api/v1/quote/car-insurance', () => {
+        const path = '/api/v1/quote/car-insurance'
+        // const path = 'http://localhost:8080/api/v1/quote/car-insurance'
+        chai.request(app)
+        .post(path)
+        .send('asd')
+        .end((err, res) => {
+            expect(res).to.not.throw(err);
+        });
+        // expect(app.use(path)).to.not.throw('404 (not found)')
     });
 });
