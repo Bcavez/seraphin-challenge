8a0205114d720f958647504b1496e10a4d6dcb32
Benoit Cavez <benoit.cavez@gmail.com>
2019-09-03 12:29:26 +0200

    added the age function in the model and also adjusted the test so it takes it

--- models/driver.js
+++ models/driver.js
@@ -2,11 +2,19 @@ module.exports = class Driver {
     constructor(birth_date, car_value) {
         this.birth_date = birth_date;
         this.car_value = car_value;
-        this.age = age();
+        this.age = this.age();
     }
 
     age() {
-
+        const birth_date = this.birth_date;
+        //birthdate has to be converted to the correct format of the Date() function
+        const birthdate_array = birth_date.split("/");
+        const formated_birthdate = `${parseInt(birthdate_array[2])}-${parseInt(birthdate_array[1])}-${parseInt(birthdate_array[0])}`;
+        //using the Date() function with the formated birthdate
+        const date_birth = new Date(formated_birthdate);
+        const date_today = new Date();
+        const age_diff = new Date(date_today - date_birth);
+        return Math.abs(age_diff.getUTCFullYear() - 1970);
     }
 
     civil_liability() {
--- test/driver.js
+++ test/driver.js
@@ -6,11 +6,11 @@ const Driver = require('../models/driver');
 describe('age computation', () => {
     it('should 25 if the driver is born 25 years ago', () => {
         const driver = new Driver("02/09/1994", 1000.00);
-        expect(driver.age()).to.equal(25);
+        expect(driver.age).to.equal(25);
     });
     it('should return 18 if the driver is born 18 years ago', () => {
         const driver = new Driver("02/09/2001", 1000.00);
-        expect(driver.age()).to.equal(18);
+        expect(driver.age).to.equal(18);
     });
 });
 
