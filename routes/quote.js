95cde75b2367ec0874d832f5f2f729cc339b0d32
Benoit Cavez <benoit.cavez@gmail.com>
2019-09-03 21:54:21 +0200

    added doc in route explaining what is supposed to be received in the request

--- routes/quote.js
+++ routes/quote.js
@@ -5,6 +5,11 @@ const validator = require('./validation/quote');
 
 const router = express.Router();
 
+// expected to receive data in JSON with the following format:
+// {
+//    "car_value": 10000.00, 
+//    "driver_birthdate": "15/10/1990"
+// }
 router.post('/api/v1/quote/car-insurance', validator.postQuote ,quoteController.postQuote);
 
 module.exports = router;
