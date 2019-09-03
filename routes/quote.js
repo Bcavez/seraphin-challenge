85e7dd481dba3059ac21c9fd74d85fdfb9dd3029
Benoit Cavez <benoit.cavez@gmail.com>
2019-09-03 09:56:13 +0200

    validating diver_age date with a regex

--- routes/quote.js
+++ routes/quote.js
@@ -5,12 +5,11 @@ const quoteController = require('../controllers/quote');
 
 const router = express.Router();
 
+const dateRegexp = /^(?:(?:31(\/)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/
+
 router.post('/api/v1/quote/car-insurance', [
     body('car_value').isFloat(),
-    // body('driver_birthdate').isBefore({date: })
+    body('driver_birthdate').matches(dateRegexp)
 ] ,quoteController.postQuote);
 
 module.exports = router;
-
-
-//check date
