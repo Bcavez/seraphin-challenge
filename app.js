ad542ab51e0c4c0ecd150dc6d0e76b38f35b50c6
Benoit Cavez <benoit.cavez@gmail.com>
2019-09-03 19:57:57 +0200

    added a app(use) in the app file which sets the header-type to json for all incoming requests in order to prevents errors from headerless curl request

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const quoteRoutes = require('./routes/quote');
 
 const app = express();
 
+// in order to prevent errors coming from curl request who do not specify header-type json
+// the header type is set to json for all incoming requests.
+app.use((req, res, next) => {
+    req.headers['content-type'] = 'application/json';
+    next();
+});
+
 app.use(bodyParser.json());
 
 app.use(quoteRoutes);
